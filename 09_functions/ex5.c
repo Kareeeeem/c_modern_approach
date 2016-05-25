1f1bb947273973a1c2dec2427a2535eb71680366
Mohammed Kareem <kareeeeem@gmail.com>
2016-05-25 13:19:43 +0200

    added some variations on ex5 and 6

--- 09_functions/ex5.c
+++ 09_functions/ex5.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int num_digits(int n);
+int num_digits_rec(int n);
 
 int main(void) {
     int i;
     printf("Enter a number: ");
     scanf("%d", &i);
-    printf("%d has %d digits\n", i, num_digits(i));
+    printf("%d has %d digits\n", i, num_digits(i * 1));
+    printf("%d has %d digits\n", i, num_digits_rec(i * 1));
     return 0;
 }
 
@@ -17,4 +19,10 @@ int num_digits(int n) {
         n /= 10;
     }
     return count;
+}
+
+int num_digits_rec(int n) {
+    if (n == 0)
+        return 0;
+    return 1 + num_digits_rec(n / 10);
 }
--- 09_functions/ex6.c
+++ 09_functions/ex6.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
+#include <math.h>
 
 int nth_digit(int number, int n);
+int nth_digit_rec(int number, int n);
+int nth_digit_math(int number, int n);
 
 int main(void) {
     int number, n;
     printf("Enter a number and the nth digit you want: ");
     scanf("%d %d", &number, &n);
     printf("Digit %d is %d\n", n, nth_digit(number, n));
+    printf("Digit %d is %d\n", n, nth_digit_rec(number, n));
+    printf("Digit %d is %d\n", n, nth_digit_math(number, n));
 }
 
 int nth_digit(int number, int n) {
     while (--n)
         number /= 10;
     return number % 10;
+}
+
+int nth_digit_rec(int number, int n) {
+    if (n <= 1)
+        return number % 10;
+    return nth_digit_rec(number / 10, --n);
+}
+
+int nth_digit_math(int number, int n) {
+    return (int) (number / pow(10, --n)) % 10;
 }
